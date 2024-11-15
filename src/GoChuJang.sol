9c6ce110ebd7b88f3e3c3db783ddab1e2d33f1d3
john <john@superblock.xyz>
2024-11-16 03:56:11 +0900

    Add

--- src/GoChuJang.sol
+++ src/GoChuJang.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.24;
 
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-import { BaseHook } from "v4-periphery/src/base/hooks/BaseHook.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary, toBeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
 
 contract GoChuJang is BaseHook {
     using PoolIdLibrary for PoolKey;
@@ -22,7 +23,7 @@ contract GoChuJang is BaseHook {
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
-            beforeInitialize: true,
+            beforeInitialize: true, // true
             afterInitialize: false,
             beforeAddLiquidity: false,
             afterAddLiquidity: false,
@@ -39,7 +40,16 @@ contract GoChuJang is BaseHook {
         });
     }
 
-    function beforeInitialize(address, PoolKey calldata, bytes calldata)
+    function poolInitialize(
+        PoolId poolId,
+        int256 scalarRoot,
+        int256 initialAnchor,
+        uint80 lnFeeRateRoot
+    ) external {
+        
+    }
+
+    function beforeInitialize(address, PoolKey calldata key, uint160 sqrtPriceX96) 
         external
         override
         returns (bytes4)
@@ -48,13 +58,27 @@ contract GoChuJang is BaseHook {
         return BaseHook.beforeInitialize.selector;
     }
 
-    
-    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata swapParams, bytes calldata)
         external
         override
         returns (bytes4, BeforeSwapDelta, uint24)
     {
-        // TODO IMPLEMENT
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+        bool exactInput = swapParams.amountSpecified < 0;
+        (Currency specified, Currency unspecified) = (swapParams.zeroForOne == exactInput) ? (key.currency0, key.currency1) : (key.currency1, key.currency0);
+        int256 specifiedAmount = exactInput ? -swapParams.amountSpecified : swapParams.amountSpecified;
+        int256 unspecifiedAmount;
+        BeforeSwapDelta returnDelta;
+        if (exactInput) {
+            unspecifiedAmount = _swap();
+            returnDelta = toBeforeSwapDelta(int128(specifiedAmount), int128(-unspecifiedAmount));
+        } else {
+            unspecifiedAmount = _swap();
+            returnDelta = toBeforeSwapDelta(int128(-specifiedAmount), int128(unspecifiedAmount));
+        }
+        return (BaseHook.beforeSwap.selector, returnDelta, 0);
+    }
+
+    function _swap() internal pure returns (int256) {
+        return 0;
     }
 }
